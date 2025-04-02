d1a1c73b2ad7c2f79425a29199a48a091287a74a
Amet <162996755+Ameteh@users.noreply.github.com>
2025-04-02 21:02:17 +0700

    9-12

---
+++ Latihan 11/src/Mavenproject1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Mavenproject1 {
+    static PriorityQueue<String> stringQueue;
+    public static void main(String[] args) {
+        stringQueue = new PriorityQueue<String>();
+        stringQueue.add("ab");
+        stringQueue.add("abcd");
+        stringQueue.add("abc");
+        stringQueue.add("a");
+        
+        //don't use iterator which may or may not
+        //show the PRiorityQueue's order
+        while(stringQueue.size() > 0)
+            System.out.println(stringQueue.remove());
+   }
+}
+ 
+    
